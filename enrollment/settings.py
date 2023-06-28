d256b0918d36f9da0a4de2cd751150fae502c902
Raymundo Melecio <meleciort1@gmail.com>
2023-06-28 09:34:00 +0000

    update code

--- enrollment/settings.py
+++ enrollment/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'student_profile',
+    'django_filters',
     'rest_framework',
     'accounts'
 
@@ -50,7 +51,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    )
+    ),
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 
     }
 
